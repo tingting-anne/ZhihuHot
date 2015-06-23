dea4bf2606a1d7dcb8ed05d469f8a6646bc10f88
anne <liutingting_fly@163.com>
2015-06-23 10:27:31 +0800

    上传项目文件，cell中图片需要使用占位图片，否则刷新很慢

---
+++ ZhiHuHot/DailyTableSectionHeader.h
@@ -0,0 +1,13 @@
+//
+//  DailyTableSectionHeader.h
+//  ZhiHuHot
+//
+//  Created by ltt.fly on 15/6/21.
+//  Copyright (c) 2015年 ltt.fly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DailyTableSectionHeader : UIView
+
+@end
---
+++ ZhiHuHot/DailyTableSectionHeader.m
@@ -0,0 +1,21 @@
+//
+//  DailyTableSectionHeader.m
+//  ZhiHuHot
+//
+//  Created by ltt.fly on 15/6/21.
+//  Copyright (c) 2015年 ltt.fly. All rights reserved.
+//
+
+#import "DailyTableSectionHeader.h"
+
+@implementation DailyTableSectionHeader
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
