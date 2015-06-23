795232241ad81157ba1c85d7a6428b2ef7c40d24
anne <liutingting_fly@163.com>
2015-06-23 19:38:40 +0800

    使热闻和主题内容可以在WebView中显示

---
+++ ZhiHuHot/ContentHeaderView.m
@@ -0,0 +1,37 @@
+//
+//  ContentHeaderViewController.m
+//  ZhiHuHot
+//
+//  Created by ltt.fly on 15/6/23.
+//  Copyright (c) 2015年 ltt.fly. All rights reserved.
+//
+
+#import "ContentHeaderViewController.h"
+
+@interface ContentHeaderViewController ()
+
+@end
+
+@implementation ContentHeaderViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
