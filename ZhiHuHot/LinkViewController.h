9a547683105edc1ec307d882bd3ad1bc0f3f08d4
anne <liutingting_fly@163.com>
2015-06-24 00:42:20 +0800

    webview能打开链接

---
+++ ZhiHuHot/LinkViewController.h
@@ -0,0 +1,13 @@
+//
+//  LinkViewController.h
+//  ZhiHuHot
+//
+//  Created by ltt.fly on 15/6/23.
+//  Copyright (c) 2015年 ltt.fly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LinkViewController : UIViewController
+
+@end
---
+++ ZhiHuHot/LinkViewController.m
@@ -0,0 +1,37 @@
+//
+//  LinkViewController.m
+//  ZhiHuHot
+//
+//  Created by ltt.fly on 15/6/23.
+//  Copyright (c) 2015年 ltt.fly. All rights reserved.
+//
+
+#import "LinkViewController.h"
+
+@interface LinkViewController ()
+
+@end
+
+@implementation LinkViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
